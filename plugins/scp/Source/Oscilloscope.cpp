9fd1efc4b075ef88a79b03b07b82f074df8c23a8
Rob Gallagher <rdgallagher@gmail.com>
2021-03-23 17:37:32 +0800

    Various UI improvements
    - Reverse position change (clockwise encoder moves down) and rate (higher)
    - Scale faster on press and turn, because turning while pressed is a pain
    - Tweak thickness and colour of UI elements

--- plugins/scp/Source/Oscilloscope.cpp
+++ plugins/scp/Source/Oscilloscope.cpp
@@ -24,42 +24,52 @@ void Oscilloscope::paint(Graphics &g) {
         const int ySquares = 10;
         const int ySquarePixels = h / ySquares;
 
-        g.fillCheckerBoard(background, xSquarePixels, ySquarePixels, Colour(0x66666666), Colours::transparentBlack);
+        g.fillCheckerBoard(background, xSquarePixels, ySquarePixels, Colour(0xff111111), Colours::transparentBlack);
 
         // draw zero voltage line (x axis)
         g.drawHorizontalLine(h / 2, 0, w);
     }
 
-    // draw channels
+    // draw trace
     for (int channel = 0; channel < _asb.getNumChannels(); channel++) {
         Colour &colour = *_colours.getUnchecked(channel);
         g.setColour(colour);
 
-        // draw line above relevant encoder
+        drawChannel(g, w, h, channel, colour);
+    }
+
+    // draw line above relevant encoder
+    for (int channel = 0; channel < _asb.getNumChannels(); channel++) {
+        Colour &colour = *_colours.getUnchecked(channel);
+        g.setColour(colour);
+
         const int gutterWidth = 30;
-        const int gutterHeight = 30;
+        const int gutterHeight = 25;
 
         const int encoderWidth = 150;
         const int encoderSpacing = 75;
         const int encoderPosition = encoderWidth + encoderSpacing;
         const int encoderStartX = gutterWidth + (encoderPosition * channel);
         const int encoderEndX = encoderStartX + encoderWidth;
-        const int encoderThickness = 5;
+        const int encoderThickness = 3;
 
         g.drawLine(encoderStartX,
                    h - gutterHeight,
                    encoderEndX,
                    h - gutterHeight,
                    encoderThickness);
-
-        // draw trace line
-        drawChannel(g, w, h, channel, colour);
     }
 
     // draw toggle background button
     g.setColour(Colours::white);
     juce::Rectangle<int> toggleBackgroundButton(w - 120, h - 45, 50, 30);
-    g.fillCheckerBoard(toggleBackgroundButton, 25, 15, Colour(0xff333333), Colours::black);
+    if (_showBackground) {
+        g.setColour(Colours::black);
+        g.fillRect(toggleBackgroundButton);
+        g.setColour(Colours::white);
+    } else {
+        g.fillCheckerBoard(toggleBackgroundButton, 25, 15, Colour(0xff222222), Colours::black);
+    }
     g.drawRect(toggleBackgroundButton);
 }
 
@@ -78,12 +88,12 @@ void Oscilloscope::drawChannel(Graphics &g, float w, float h, int channel, Colou
 
         // scale val to encoder value
         const float pressedEncoderValue = _processor.getParameter(Percussa::sspLast - (4 - channel));
-        const float scalingFactor = pow(2, pressedEncoderValue / 4);
+        const float scalingFactor = pow(2, pressedEncoderValue);
         val = val * scalingFactor;
 
         // center val on the screen (0.0f -> 1.0f)
         const float encoderValue = _processor.getParameter(channel);
-        const float positionOffset = encoderValue / 8;
+        const float positionOffset = -encoderValue / 4;
         val = 1.0f - (val + 1.0f + positionOffset) * 0.5f;
 
         // scale val to screen height in pixels
